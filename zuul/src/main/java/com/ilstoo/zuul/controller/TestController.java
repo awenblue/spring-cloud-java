14fab339eca0b42f715d7bde249a59f1f5449344
awen_blue <awen_blue@126.com>
2020-05-05 19:48:03 +0800

    update project

---
+++ zuul/src/main/java/com/ilstoo/zuul/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.ilstoo.zuul.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yihaowen
+ * @create 2020-05-05 19:45
+ */
+@RestController
+public class TestController {
+
+    
+
+
+}
