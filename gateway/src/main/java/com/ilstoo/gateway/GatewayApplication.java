52be17c68d89b2e8d38a55f89c90fa8d6188ec8a
awen_blue <awen_blue@126.com>
2020-05-06 19:37:10 +0800

    add spring cloud gateway

---
+++ gateway/src/main/java/com/ilstoo/gateway/GatewayApplication.java
@@ -0,0 +1,22 @@
+package com.ilstoo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+/**
+ * @author yihaowen
+ * @create 2020-05-05 14:26
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
---
+++ gateway/src/main/java/com/ilstoo/gateway/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.ilstoo.gateway.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yihaowen
+ * @create 2020-05-05 19:45
+ */
+@RestController
+public class TestController {
+
+    @Value("${echo.account}")
+    private String account;
+
+    @GetMapping("/echo")
+    @ResponseBody
+    public String echo() {
+        return account;
+    }
+}
