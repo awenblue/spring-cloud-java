f56c8a5b5d9da06438af4b73ebc9aea38c6fd1d6
awen_blue <awen_blue@126.com>
2020-05-09 10:18:27 +0800

    auth2 jwt

--- authentication/src/main/java/com/ilstoo/authentication/security/JWTTokenEnhancer.java
+++ authentication/src/main/java/com/ilstoo/authentication/security/JWTTokenEnhancer.java
@@ -1,28 +1,28 @@
-//package com.ilstoo.authentication.security;
-//
-//
-//import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
-//import org.springframework.security.oauth2.common.OAuth2AccessToken;
-//import org.springframework.security.oauth2.provider.OAuth2Authentication;
-//import org.springframework.security.oauth2.provider.token.TokenEnhancer;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//public class JWTTokenEnhancer implements TokenEnhancer {
-//
-//    private String getOrgId(String userName){
-//        return userName;
-//    }
-//
-//    @Override
-//    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
-//        Map<String, Object> additionalInfo = new HashMap<>();
-//        String orgId =  getOrgId(authentication.getName());
-//
-//        additionalInfo.put("organizationId", orgId);
-//
-//        ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
-//        return accessToken;
-//    }
-//}
+package com.ilstoo.authentication.security;
+
+
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JWTTokenEnhancer implements TokenEnhancer {
+
+    private String getOrgId(String userName){
+        return userName;
+    }
+
+    @Override
+    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+        Map<String, Object> additionalInfo = new HashMap<>();
+        String orgId =  getOrgId(authentication.getName());
+
+        additionalInfo.put("organizationId", orgId);
+
+        ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
+        return accessToken;
+    }
+}
