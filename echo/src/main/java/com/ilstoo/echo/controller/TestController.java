1e0fb9fea02d4e3cb8d343148686da7e2832452a
awen_blue <awen_blue@126.com>
2020-05-19 15:52:37 +0800

    add bus, auto refresh config

--- echo/src/main/java/com/ilstoo/echo/controller/TestController.java
+++ echo/src/main/java/com/ilstoo/echo/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.ilstoo.echo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @create 2020-05-05 19:45
  */
 @RestController
+@RefreshScope
 public class TestController {
+
+    @Value("${echo.account}")
+    private String account;
 
     @GetMapping("/echo")
     @ResponseBody
     public String echo() {
-        return "echo";
+        return account;
     }
 }
